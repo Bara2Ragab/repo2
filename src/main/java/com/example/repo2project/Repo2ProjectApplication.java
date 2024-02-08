aa539e0041143c62e3d9cef49f29219fb6f60a6c
Megaport <mr.tbab@hotmail.com>
2024-02-08 17:41:41 +0100

    push project2

---
+++ src/main/java/com/example/repo2project/Repo2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.repo2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Repo2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Repo2ProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/repo2project/Repo2Test.java
@@ -0,0 +1,7 @@
+package com.example.repo2project;
+
+public class Repo2Test {
+    public void test(){
+        System.out.println("test repo 2");
+    }
+}
